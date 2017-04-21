79f6af48e34379f1152f23362686fe4a2989bb0f
Smyriem <saidi.myriem@gmail.com>
2017-04-21 17:51:50 +0200

    create content.js

---
+++ content.js
@@ -0,0 +1,17 @@
+InboxSDK.load('1', 'EmailScheduler').then(function(sdk){
+
+	// the SDK has been loaded !
+	sdk.Compose.registerComposeViewHandler(function(composeView){
+
+		// a compose view has come into existence !
+		composeView.addButton({
+			title: "Send later(Ctrl+Alt+L)",				
+			iconUrl : chrome.extension.getURL('/icons/calendar-timeout-128.png'),			
+			onClick: function(event) {
+				event.composeView.insertTextIntoBodyAtCursor('Hello');
+			},
+		});
+
+	});
+
+});
